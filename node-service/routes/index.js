2512fa7e7d7abe4916441752132ef1b8445ee3a8
Mickaël Royer <mickael-royer@users.noreply.github.com>
2023-01-16 16:50:48 +0000

    Add authRequired() to index

--- node-service/routes/index.js
+++ node-service/routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 const { requiresAuth } = require('express-openid-connect');
 
 /* GET home page. */
-router.get("/", requiresAuth, function (req, res, next) {
+router.get("/", requiresAuth(), function (req, res, next) {
   res.render("index", { title: "Container Apps Demo" });
 });
 
